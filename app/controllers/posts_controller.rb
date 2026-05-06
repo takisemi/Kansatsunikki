049ea248cb69d7f377e7ddc1db93d64ee4c5914c
takisemi <11sato3323@gmail.com>
2026-05-06 17:56:30 +0900

    修正：自分の記事だけ確認・編集可。各ビューのwidth。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,18 @@
 class PostsController < ApplicationController
   skip_before_action :require_login, only: [:index, :show]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :require_login  # ログイン必須にする
 
   def index
-    @posts = Post.all
+    # 全ての投稿ではなく、ログインユーザーの投稿のみ取得
+    @posts = current_user.posts.order(created_at: :desc)
   end
 
   def show
   end
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def create
@@ -24,9 +26,12 @@ class PostsController < ApplicationController
   end
 
   def edit
+      # 自分の投稿のみ取得（他人の投稿は編集できないようにする）
+    @post = current_user.posts.find(params[:id])
   end
 
   def update
+    @post = current_user.posts.find(params[:id])
     if @post.update(post_params)
       redirect_to @post, notice: '更新しました！'
     else
@@ -35,14 +40,17 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post.destroy
+    @post = current_user.posts.find(params[:id])
+    @post.destroy!
     redirect_to posts_url, notice: '削除しました！'
   end
 
   private
 
   def set_post
-    @post = Post.find(params[:id])
+    @post = current_user.posts.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to posts_path, alert: '記事が見つかりません'
   end
 
   def post_params
