c34cd98761d97eead0795f2d232930d0ccac5301
takisemi <11sato3323@gmail.com>
2026-05-24 23:42:06 +0900

    fix:CIで引っかかった箇所を修正

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,9 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  root 'static_pages#top'
+  root "static_pages#top"
   resources :users, only: %i[new create destroy]
-  get 'login', to: 'user_sessions#new'
+  get 'login', to: "user_sessions#new"
   post 'login', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
   resources :posts
